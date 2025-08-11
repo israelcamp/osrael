fabb4a0783f309e845a0cb20fa861c6465390f54
Israel Campiotti <israelcampiotti@gmail.com>
2025-08-11 07:58:29 -0300

    more plymouth setup

--- plymouth/setup.sh
+++ plymouth/setup.sh
@@ -8,6 +8,8 @@
 # change /etc/default/grub to have 
 # GRUB_CMDLINE_LINUX_DEFAULT=" quiet splash"
 # sudo cp -r ~/osrael/plymouth/themes/osrael /usr/share/plymouth/themes/
+# for systemd change /boot/loader/entries/2025-08-10_13-56-55_linux.conf 
+# add quiet splash at the end of options
 sudo plymouth-set-default-theme -R osrael
 
 # regenerat grub sudo grub-mkconfig -o /boot/grub/grub.cfg
