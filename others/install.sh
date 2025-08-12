89ce563f588424b1ee11d84d26c6dd0b975205f3
Israel Campiotti <israelcampiotti@gmail.com>
2025-08-12 17:55:41 -0300

    add docker and docker-compose

--- others/install.sh
+++ others/install.sh
@@ -1,3 +1,3 @@
-yay -S lazygit openssh git ripgrep viu zip unzip brightnessctl
+yay -S lazygit openssh git ripgrep viu zip unzip brightnessctl docker docker-compose
 yay -S --needed xdg-desktop-portal xdg-desktop-portal-gnome
 curl -LsSf https://astral.sh/uv/install.sh | sh
