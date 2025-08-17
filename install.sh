6de9c186cd7e016eb78aa11d8cbdb61dc7cc9436
Israel Campiotti <israelcampiotti@gmail.com>
2025-08-17 20:14:47 -0300

    adding config to yay during install

--- install.sh
+++ install.sh
@@ -1,4 +1,17 @@
 cd yay && ./install.sh && cd ..
+
+mkdir -p ~/.config/yay
+cat > ~/.config/yay/config.json <<'EOF'
+{
+  "sudoloop": true,
+  "cleanAfter": true,
+  "cleanMenu": true,
+  "diffMenu": false,
+  "answerclean": "All",
+  "noconfirm": true
+}
+EOF
+
 cd zsh && ./install.sh && ./setup.sh && cd ..
 cd ghostty && ./install.sh && ./setup.sh && cd ..
 cd hyprland && ./install.sh && ./setup.sh && cd ..
@@ -9,3 +22,5 @@ cd waybar && ./install.sh && ./setup.sh && cd ..
 cd nvim && ./install.sh && ./setup.sh && cd ..
 cd files && ./install.sh && cd ..
 cd walker && ./install.sh && ./setup.sh && cd ..
+
+rm ~/.config/yay/config.json
