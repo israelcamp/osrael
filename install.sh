5742f8e1cbe478bb4031eb38866f335113b15972
Israel Campiotti <israelcampiotti@gmail.com>
2025-08-17 20:08:19 -0300

    adding install for the whole setup

---
+++ install.sh
@@ -0,0 +1,11 @@
+cd yay && ./install.sh && cd ..
+cd zsh && ./install.sh && ./setup.sh && cd ..
+cd ghostty && ./install.sh && ./setup.sh && cd ..
+cd hyprland && ./install.sh && ./setup.sh && cd ..
+cd others && ./install.sh && ./setup.sh && cd ..
+cd brave && ./install.sh && cd ..
+cd tlp && ./install.sh && ./setup.sh && cd ..
+cd waybar && ./install.sh && ./setup.sh && cd ..
+cd nvim && ./install.sh && ./setup.sh && cd ..
+cd files && ./install.sh && cd ..
+cd walker && ./install.sh && ./setup.sh && cd ..
