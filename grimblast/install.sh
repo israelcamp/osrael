89bd0eebbb970ed428433c7ff2951b524be9362d
Israel Campiotti <israelcampiotti@gmail.com>
2025-08-18 15:54:52 -0300

    add grimblast for screenshot

---
+++ grimblast/install.sh
@@ -0,0 +1,3 @@
+yay -S grim slurp wl-clipboard jq libnotify hyprpicker
+yay -S grimblast-git
+
--- install.sh
+++ install.sh
@@ -17,6 +17,7 @@ cd ghostty && ./install.sh && ./setup.sh && cd ..
 cd hyprland && ./install.sh && ./setup.sh && cd ..
 cd others && ./install.sh && ./setup.sh && cd ..
 cd brave && ./install.sh && cd ..
+cd grimblast && ./install.sh && cd ..
 cd tlp && ./install.sh && ./setup.sh && cd ..
 cd waybar && ./install.sh && ./setup.sh && cd ..
 cd nvim && ./install.sh && ./setup.sh && cd ..
